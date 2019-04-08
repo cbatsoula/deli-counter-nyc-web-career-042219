511c00623cab9223f701d9569ea6cbe60de330e4
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-08 20:13:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,20 +14,6 @@ def line(array)
 end
 
 
-def line (katz_deli)
-
-  if katz_deli.length == 0
-    puts "The line is currently empty."
-    
-  else katz_deli.length >= 1
-    puts "The line is currently: #{other_deli}."
-    
-    katz_deli.each_with_index do |value, index|
-      message = "The line is currently: #{index.to_i+1}. #{value} "
-      puts message
-end
-end
-end
 
 def take_a_number(katz_deli, name)
    
